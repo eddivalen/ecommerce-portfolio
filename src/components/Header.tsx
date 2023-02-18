62dd1ab4053aab35033288038e27c04f2bd774cb
Eddy Valencia <eddyvalencia8@gmail.com>
2023-02-18 11:51:18 -0600

    Added: replaced images, and responsive for header and topbar components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,79 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import {Flex, Box, Container} from '@chakra-ui/react'
+import {Flex, Box, Container, IconButton, Menu, MenuButton, MenuItem, MenuList, Show} from '@chakra-ui/react'
+import { HamburgerIcon} from '@chakra-ui/icons';
+
+const menu = [
+  {
+    title: 'About Us',
+    href: '#'
+  },
+  {
+    title: 'Woman',
+    href: '#'
+  },
+  {
+    title: 'Men',
+    href: '#'
+  },
+  {
+    title: 'Beauty',
+    href: '#'
+  },
+  {
+    title: 'Accesories',
+    href: '#'
+  },
+  {
+    title: 'Blog',
+    href: '#'
+  },
+  {
+    title: 'Contact',
+    href: '#'
+  },
+]
 
 export function Header() {
   return (
     <Box w="100%" as="header" borderBottom="solid 2px" borderColor="gray.100">
      <Container as={Flex} justifyContent="space-between" alignItems="center" size="lg">
-        <Box margin="1rem 0">
-          <Image src="/logo2.svg" alt="" width="100" height="48" />
-        </Box>
-        <nav>
-          <Flex as="ul" listStyleType="none" gap="2rem">
-            <li><Link href="#">About Us</Link></li>
-            <li><Link href="#">Woman</Link></li>
-            <li><Link href="#">Men</Link></li>
-            <li><Link href="#">Beauty</Link></li>
-            <li><Link href="#">Accesories</Link></li>
-            <li><Link href="#">Blog</Link></li>
-            <li><Link href="#">Contact</Link></li>
-          </Flex>
-        </nav>
+        <Flex gap="0.5rem" margin="1rem 0">
+          <Show below='md'>
+            <Box as="nav">
+                <Menu>
+                  <MenuButton
+                    as={IconButton}
+                    aria-label='Options'
+                    icon={<HamburgerIcon />}
+                    variant='outline'
+                  />
+                  <MenuList>
+                    {
+                    menu.map((item, index) => {
+                      return <MenuItem key={index}>
+                              <Link href={item.href}>{item.title}</Link>
+                            </MenuItem>
+                    })
+                    }
+                  </MenuList>
+                </Menu>
+            </Box>
+          </Show>
+          <Image src="/logo.svg" alt="" width="100" height="48" />
+        </Flex>
+        <Show above="md">
+          <Box as="nav">
+              <Flex as="ul" listStyleType="none" gap="2rem">
+                {
+                  menu.map((item, index) => {
+                    return <li key={index}><Link href={item.href}>{item.title}</Link></li>
+                    })
+                }
+              </Flex>
+          </Box>
+        </Show>
+ 
         <div className="commerce-menu">
           <Flex as="ul" listStyleType="none" gap="1.5rem">
             <li>
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -10,9 +10,9 @@ export function TopBar() {
         <Flex gap="1.5rem">
           <Flex as={Link} href="#" alignItems="center" gap="0.5rem"><PhoneIcon /> +38 (050) 12 34 567</Flex>
         
-          <Flex as={Link} href="#" alignItems="center" gap="0.5rem"><EmailIcon /> Ukraine, Kyiv, Khreschtayk 1</Flex>
+          <Flex display={{base: 'none', sm:'flex'}} as={Link} href="#" alignItems="center" gap="0.5rem"><EmailIcon /> Ukraine, Kyiv, Khreschtayk 1</Flex>
       
-          <Flex as={Link} href="#" alignItems="center" gap="0.5rem"><CalendarIcon /> All week 24/7</Flex>
+          <Flex display={{base: 'none', sm:'flex'}} as={Link} href="#" alignItems="center" gap="0.5rem"><CalendarIcon /> All week 24/7</Flex>
         </Flex>
         <Flex gap="1rem">
           <Link href="#"><Image src="/ico-facebook.svg" alt="Search Icon" width="24" height="24" /></Link>
