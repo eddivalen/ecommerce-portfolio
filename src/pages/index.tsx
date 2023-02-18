43d653c3ccc8489024ae2596d280712cd4b01d99
Eddy Valencia <eddyvalencia8@gmail.com>
2023-02-17 18:13:36 -0600

    Added: Extract advantage section component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,12 @@ import Head from 'next/head'
 
 import { Box, Container, Flex } from "@chakra-ui/react";
 
-import  Header  from '../components/Header'
-import TopBar  from '../components/TopBar'
+import Header from '../components/Header'
+import TopBar from '../components/TopBar'
 import { HomeHeroCategories } from '@/components/HomeHeroCategories'
 import { Categories } from '@/models/Categories'
 import { AdvantageItem } from "@/components/AdvantageItem";
+import { AdvantageSection } from "@/components/AdvantageSection";
 
 type Product = {
   id: number;
@@ -29,7 +30,11 @@ type Props = {
 }
 
 
-export default function Home( { products, categories } : Props) {
+
+
+
+
+export default function Home({ products, categories }: Props) {
   return (
     <>
       <Head>
@@ -38,31 +43,27 @@ export default function Home( { products, categories } : Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <TopBar />
-        <Box marginBottom="2rem">
-          <Header />
-        </Box>
+      <TopBar />
+      <Box marginBottom="2rem">
+        <Header />
+      </Box>
       <main>
         <Container size="lg">
           <HomeHeroCategories categories={categories} />
-        <Flex justifyContent="space-between" margin="2rem 0">
-          <AdvantageItem title="Free Shipping" content="On all UA order or orders above $100" icon="/ico-truck.svg" />  
-          <AdvantageItem title="30 days return" content="Simply return it within 30 days for an exchange" icon="/ico-return.svg" />  
-          <AdvantageItem title="Support 24/7" content="Contact us 24 hours a day, 7 days a week" icon="/ico-support.svg" />  
-        </Flex>            
+          <AdvantageSection />
         </Container>
-  
+
       </main>
     </>
   )
 }
 
-export async function getServerSideProps(context: GetServerSidePropsContext){
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   const products = await fetch('https://fakestoreapi.com/products')
-        .then(res => res.json());
+    .then(res => res.json());
 
   const categories = await fetch('https://fakestoreapi.com/products/categories')
-        .then(res => res.json());
+    .then(res => res.json());
   return {
     props: {
       products,
