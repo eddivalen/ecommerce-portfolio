4cc431c8bd8838c893781047034ed4f70b0ae68a
Eddy Valencia <eddyvalencia8@gmail.com>
2023-02-18 15:20:33 -0600

    Added: responsive styles for HomeHeroCategories component

--- src/components/HomeHeroCategories.tsx
+++ src/components/HomeHeroCategories.tsx
@@ -13,22 +13,36 @@ type Props = {
 
 export function HomeHeroCategories({categories}: Props){
   return (
-    <Grid templateColumns="540px 255px 255px" templateRows="200px 260px"  gap="30px">
-      {categories.map( (cat,key) => {
+    <Grid templateColumns={{
+      base: '1fr 1fr',
+      sm: '540px 255px 255px'
+    }} templateRows={{
+      base: '130px 154px 130px',
+      sm: '200px 260px'
+    }}  gap={{
+      base: '0.5rem',
+      sm: '30px'
+    }} templateAreas={{
+      base: `
+        "cat1 cat1"
+        "cat2 cat3"
+        "cat4 cat4"
+      `,
+      sm: `
+        "cat1 cat2 cat3"
+        "cat1 cat4 cat4"
+      `,
+    }}>
+      {categories.map( (cat,index) => {
         const slug = slugify(cat);
         const imageUrl = `/pic-categories-${slug}.jpg`
 
-        const gridItemProps: GridItemProps = {
-          position: 'relative',
-          w: '100%',
-          h: '100%',
-        }
-        if(key === 0)
-          gridItemProps.rowSpan = 2;
-        if(key === categories.length -1 )
-          gridItemProps.colSpan = 2;
-          
-        return  <GridItem {...gridItemProps} key={key}><Image src={imageUrl} fill={true} alt={cat} />
+        // To do fix the image size crop issue
+        return  <GridItem fontSize={{
+          base: '0.85rem',
+          sm: '1rem'
+        }} position="relative" w="100%" h="100%" gridArea={`cat${index + 1}`} key={index}><Image 
+          src={imageUrl} style={{objectFit:'cover'}} fill={true} alt={cat} />
           <CenteredLabel>{cat}</CenteredLabel> 
         </GridItem>
         })}
